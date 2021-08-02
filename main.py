a772d77e5e78156aec4d1d4de55f5effa89c98a9
Anusheel Shukla <anusheellearn@gmail.com>
2021-08-02 13:46:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from prettytable import PrettyTable
+Table_Heading = PrettyTable(["S.No.","Number", "Multiple", "Result"])
 def Creating_Multiplication_Table(multiplicant,multiple,):
     for i in range(1,11):
         multiple+= 1
         Result = multiplicant * multiple
-        Display = str(multiplicant) + " * " + str(multiple) + " = " + str(Result)
-        print("______________________________________________________________________")
-        print(Display)
+        Table_Heading.add_row([str(multiple),str(multiplicant) , str(multiple), str(Result)])
+    print(Table_Heading)
+
 Multiple = 0
 Multiplicant = int(input("Multiplicand: "))
 Creating_Multiplication_Table(Multiplicant,Multiple)
